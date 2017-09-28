93412a08d48c0d57f0fca460d96bfb8a0e3c2695
Matt Shirley <me@mattshirley.net>
2017-09-27 23:20:57 -0700

    Updated PropTypes dep

--- src/meta_tags.js
+++ src/meta_tags.js
@@ -1,5 +1,6 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 //function to append childrens on a parent
 function appendChild(parent, childrens) {
