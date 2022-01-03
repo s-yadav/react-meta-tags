f25e43ab3e176245471dbb2a3ff28e2812824b65
Alex <44344839+alex-shul@users.noreply.github.com>
2022-01-03 20:14:31 +0300

    Create server.d.ts

---
+++ server.d.ts
@@ -0,0 +1,11 @@
+import { ExtractFunction } from './index';
+
+export interface MetaTagsInstance {
+  extract: ExtractFunction;
+  renderToString: () => string;
+  getTags: () => React.ReactElement[];
+}
+
+const MetaTagsServer: () => MetaTagsInstance
+
+export default MetaTagsServer;
